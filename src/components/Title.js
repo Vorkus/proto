c4e859995b8a775c0b66e74c7ccd82df621ab75b
Gerard Pinyot <gerpincas@gmail.com>
2023-09-22 01:24:19 +0200

    hotfix

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,10 +3,10 @@ import SearchWidget from "./SearchWidget";
 
 export default function Title() {
     return (
-        <section className={"container-fluid title-container d-flex align-items-end justify-content-between"}>
-            <h1>Models Catalog</h1>
+        <section className={"container-fluid title-container d-flex align-items-center justify-content-between"}>
+            <h1 className={"ml-4"}>Models Catalog</h1>
             <SearchWidget />
-            <img className={"brand-logo pr-4"} src={"https://app.anything.world/static/media/logo_new.e3df371f.svg"}/>
+            <img className={"brand-logo pr-4"} src={"https://app.anything.world/static/media/logo_new.e3df371f.svg"} alt={"brand logo"}/>
         </section>
     );
 }
