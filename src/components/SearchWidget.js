d7426e004b44a6c988b7fc36eb8c72f6d3b48ebd
Gerard Pinyot <gerpincas@gmail.com>
2023-09-22 11:49:52 +0200

    Hotfix to header

--- src/components/SearchWidget.js
+++ src/components/SearchWidget.js
@@ -25,26 +25,35 @@ export default function SearchWidget() {
     }
 
     return (
-        <form id={"modelSearch"} className={"d-flex search-form"}>
-            <label id={"modelSearchLabel"} htmlFor={"modelSearchInput"} className={"form-label"} hidden={true}>Model Search</label>
-            <input
-                id={"modelSearchInput"}
-                className={"search-input form-control"}
-                onFocus={handleInputFocus}
-                onBlur={handleInputBlur}
-                onChange={handleInputChange}
-                placeholder={"     "}
-                value={inputValue}
-                required
-            />
-            <FontAwesomeIcon
-                className={"modelSearchRemove"}
-                icon={icon({name: "xmark"})}
-                style={{visibility: cancelButtonVisibility}}
-                onMouseDown={handleRemoveMouseDown}
-            ></FontAwesomeIcon>
-            <FontAwesomeIcon className={"modelSearchButton"} icon={icon({name: 'magnifying-glass'})}></FontAwesomeIcon>
-        </form>
+        <>
+            <form id={"modelSearch"} className={"d-flex search-form mb-2"}>
+                <label id={"modelSearchLabel"} htmlFor={"modelSearchInput"} className={"form-label"} hidden={true}>Model Search</label>
+                <input
+                    id={"modelSearchInput"}
+                    className={"search-input form-control"}
+                    onFocus={handleInputFocus}
+                    onBlur={handleInputBlur}
+                    onChange={handleInputChange}
+                    placeholder={"     "}
+                    value={inputValue}
+                    required
+                />
+                <FontAwesomeIcon
+                    className={"modelSearchRemove"}
+                    icon={icon({name: "xmark"})}
+                    style={{visibility: cancelButtonVisibility}}
+                    onMouseDown={handleRemoveMouseDown}
+                ></FontAwesomeIcon>
+                <FontAwesomeIcon className={"modelSearchButton"} icon={icon({name: 'magnifying-glass'})}></FontAwesomeIcon>
+            </form>
+            <div className={"d-flex justify-content-end help-badges mb-2 flex-wrap"}>
+                <span className="badge rounded-pill m-1">Statue</span>
+                <span className="badge rounded-pill m-1">Music</span>
+                <span className="badge rounded-pill m-1">Animal</span>
+                <span className="badge rounded-pill m-1">Armor</span>
+                <span className="badge rounded-pill m-1">Book</span>
+            </div>
+        </>
     );
 }
 
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,10 +3,17 @@ import SearchWidget from "./SearchWidget";
 
 export default function Title() {
     return (
-        <section className={"container-fluid title-container d-flex align-items-center justify-content-between"}>
-            <h1 className={"title ml-4"}>Models Catalog</h1>
-            <SearchWidget />
-            <img className={"brand-logo pr-4"} src={"https://app.anything.world/static/media/logo_new.e3df371f.svg"} alt={"brand logo"}/>
+        <section className={"container-fluid title-container d-flex align-items-center justify-content-between row m-0"}>
+            <div className={"col-md-6 col-12 text-md-start text-center title-container m-md-0 my-2"}>
+                <h1><strong>Models Catalog</strong></h1>
+                <p>Create living 3D worlds using open-source 3D models animated by AI.</p>
+            </div>
+            <div className={"offset-lg-2 col-lg-4 offset-md-1 col-md-5 col-12 search-container m-md-0 my-2"}>
+                <SearchWidget />
+            </div>
+            {/*<div className={"col text-end"}>*/}
+            {/*    <img className={"brand-logo pr-4"} src={"https://app.anything.world/static/media/logo_new.e3df371f.svg"} alt={"brand logo"}/>*/}
+            {/*</div>*/}
         </section>
     );
 }
