3fd8a6bd95200d7127e061d198ce22e773415e5f
Gerard Pinyot <gerpincas@gmail.com>
2023-09-22 11:01:49 +0200

    Search complete

--- src/components/SearchWidget.js
+++ src/components/SearchWidget.js
@@ -1,11 +1,50 @@
 import '../assets/SearchWidget.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+import {useState} from "react";
 
 export default function SearchWidget() {
+    const [inputValue, setinputValue] = useState('');
+    const [cancelButtonVisibility, setcancelButtonVisibility] = useState('hidden');
+
+    function handleInputFocus() {
+        setcancelButtonVisibility('visible');
+    }
+
+    function handleInputBlur() {
+        setcancelButtonVisibility('hidden');
+    }
+
+    function handleRemoveMouseDown(event) {
+        event.preventDefault();
+        setinputValue('');
+    }
+
+    function handleInputChange(event) {
+        setinputValue(event.target.value);
+    }
+
     return (
-        <form id={"modelSearch"}>
+        <form id={"modelSearch"} className={"d-flex search-form"}>
             <label id={"modelSearchLabel"} htmlFor={"modelSearchInput"} className={"form-label"} hidden={true}>Model Search</label>
-            <input id={"modelSearchInput"} className={"search-input form-control"} placeholder={"     "} />
-            <button>Search</button>
+            <input
+                id={"modelSearchInput"}
+                className={"search-input form-control"}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                onChange={handleInputChange}
+                placeholder={"     "}
+                value={inputValue}
+                required
+            />
+            <FontAwesomeIcon
+                className={"modelSearchRemove"}
+                icon={icon({name: "xmark"})}
+                style={{visibility: cancelButtonVisibility}}
+                onMouseDown={handleRemoveMouseDown}
+            ></FontAwesomeIcon>
+            <FontAwesomeIcon className={"modelSearchButton"} icon={icon({name: 'magnifying-glass'})}></FontAwesomeIcon>
         </form>
     );
 }
+
--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ import SearchWidget from "./SearchWidget";
 export default function Title() {
     return (
         <section className={"container-fluid title-container d-flex align-items-center justify-content-between"}>
-            <h1 className={"ml-4"}>Models Catalog</h1>
+            <h1 className={"title ml-4"}>Models Catalog</h1>
             <SearchWidget />
             <img className={"brand-logo pr-4"} src={"https://app.anything.world/static/media/logo_new.e3df371f.svg"} alt={"brand logo"}/>
         </section>
