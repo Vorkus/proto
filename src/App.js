75216721cf695771879239ec7c05446c3c3df071
Gerard Pinyot <gerpincas@gmail.com>
2023-09-26 17:09:09 +0200

    Normalizing for environments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {createBrowserRouter, createHashRouter, RouterProvider} from "react-router-dom";
+import {createHashRouter, RouterProvider} from "react-router-dom";
 import Gallery from "./routes/Gallery";
 import Home from "./routes/Home";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/Proto",
         element: <Home />,
